bda64dad2b7da1a3a6cec69ff06f2196f5dad1ad
Tejas Borate <boratetejas62@gmail.com>
2022-10-03 00:44:58 +0530

    Navbar changes made

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
+import NavbarHeader from '../components/NavbarHeader'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
