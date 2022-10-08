4993abf500f9c28118c152bf43756ae75dcf3b58
Tejas Borate <boratetejas62@gmail.com>
2022-10-08 21:54:07 +0530

    Next Nprogress bar ui progress bar added:

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
-import NavbarHeader from '../components/NavbarHeader'
+import NavbarHeader from '../components/NavbarHeader';
+import NextNProgress from "nextjs-progressbar";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <>
+    <NextNProgress
+      color="#AE1EED"
+      startPosition={0.3}
+      stopDelayMs={200}
+      height={4}
+      showOnShallow={true}
+      options={{ easing: "ease", speed: 500 }}
+    />
     <Component {...pageProps} />
   </>
 }
